4d981988481af842bbcd642f252863032c206461
Kuark <72679319+XiaoYi-byte@users.noreply.github.com>
2021-12-18 19:03:46 +0800

    增加了需求分析归约登录注册等内容

--- backend/src/main/java/com/example/backend/compile/ExecuteStringSourceService.java
+++ backend/src/main/java/com/example/backend/compile/ExecuteStringSourceService.java
@@ -18,7 +18,7 @@ public class ExecuteStringSourceService {
 
     /* 负责执行客户端代码的线程池，根据《Java 开发手册》不可用 Executor 创建，有 OOM 的可能 */
     private static final ExecutorService pool = new ThreadPoolExecutor(N_THREAD, N_THREAD,
-            0L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(N_THREAD));
+            0L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(N_THREAD));
 
     private static final String WAIT_WARNING = "服务器忙，请稍后提交";
     private static final String NO_OUTPUT = "Nothing.";
@@ -50,7 +50,6 @@ public class ExecuteStringSourceService {
                 return JavaClassExecutor.execute(classBytes, systemIn);
             }
         };
-
         Future<String> res = null;
         try {
             res = pool.submit(runTask);
