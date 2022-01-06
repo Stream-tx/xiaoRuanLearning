fb0968aaeaca2445b33fbb80c30d740f3544093a
Davis_Wang <1393225956@qq.com>
2022-01-06 17:56:04 +0800

    random problem & records

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -8,8 +8,48 @@
             <el-divider></el-divider>
             题目内容
           </el-tab-pane>
-          <el-tab-pane label="题解">题解</el-tab-pane>
-          <el-tab-pane label="提交记录">提交记录</el-tab-pane>
+          <el-tab-pane label="题解">
+
+          </el-tab-pane>
+          <el-tab-pane label="提交记录">
+            <div class="status" >
+              <el-table
+                  :data="tableData"
+                  style="width: 100%;left: 15%;"
+                  :row-class-name="tableRowClassName"
+              >
+                <el-table-column label="id" align="center" prop="id" v-if="false" />
+                <el-table-column
+                    prop="result"
+                    label="提交结果"
+                    width="100">
+                </el-table-column>
+                <el-table-column
+                    prop="runtime"
+                    label="执行用时"
+                    sortable
+                    width="100">
+                </el-table-column>
+                <el-table-column
+                    prop="memory"
+                    label="内存消耗"
+                    sortable
+                    width="100">
+                </el-table-column>
+                <el-table-column
+                    prop="language"
+                    label="语言"
+                    width="50">
+                </el-table-column>
+                <el-table-column
+                    prop="time"
+                    label="提交时间"
+                    sortable
+                    width="150">
+                </el-table-column>
+              </el-table>
+            </div>
+          </el-tab-pane>
         </el-tabs>
       </el-aside>
       <el-main>
@@ -19,11 +59,8 @@
     <el-divider></el-divider>
     <el-footer>
       <el-row :gutter="20">
-        <el-col :span="4"><div class="grid-content">
-          <el-button size="middle">题目列表</el-button>
-        </div></el-col>
         <el-col :span="2" :offset="2"><div class="grid-content">
-          <el-button size="middle">随机一题</el-button>
+          <el-button size="middle" @click="random()">随机一题</el-button>
         </div></el-col>
         <el-col :span="2" :offset="1"><div class="grid-content">
           <el-button size="middle" icon="el-icon-arrow-left">上一页</el-button>
@@ -59,18 +96,42 @@ export default {
     return {
       id:'',
       nowPage:'1',
-      maxPage:'200'
+      maxPage:'200',
+      tableData:[]
     };
   },
   components:{
     Submit,
   },
   methods: {
+    random(){
+      //这里需要改成题目总数！
+      this.$router.push("/hdoj/bank/q/"+(Math.floor(Math.random() * 3 + 1)).toString());
+    },
+    // loaddata(){
+    //   this.$axios
+    //       .get("")
+    //       .then(response => {
+    //         let tableData1 = [];
+    //         /**
+    //          * 这里需要将拿到的对象转为数组，进行赋值，这样才不会类型错误
+    //          */
+    //         for(let i in response.data.data.XXX){
+    //           tableData1.push(response.data.data.XXX[i]);
+    //         }
+    //         this.tableData = tableData1;
+    //         //console.log(response);
+    //       })
+    //       .catch(error => {
+    //         console.log(error);
+    //       });
+    // },
   },
   mounted() {
     this.id=this.$route.params.id;
     this.$refs.nowPage.$el.innerHTML=this.id;
     this.$refs.maxPage.$el.innerHTML=200;
+    // this.loaddata();
   }
 }
 </script>
