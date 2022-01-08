22a60bb2122d7661588c318f9f30f6207bc6e6af
Kuark <72679319+XiaoYi-byte@users.noreply.github.com>
2022-01-08 09:52:57 +0800

    修改myinfo界面

--- frontend/src/views/MyInfo.vue
+++ frontend/src/views/MyInfo.vue
@@ -1,58 +1,48 @@
 <template>
-
   <div class="common-layout">
-
     <el-dialog style="width:650px;margin:auto;margin-top:20px;margin-down:30px;top:-150px;" v-model="dialogFormVisible">
       <el-card style="width:650px;margin:auto;margin-top:20px;margin-down:30px;">
         <el-card-content style="text-align: left">
-          <h1>修改个人信息</h1>
-
+          <h1>修改个⼈信息</h1>
           <div class="course-title">昵称：
-            <el-input class="mb-4" v-model="this.nameAlter" placeholder="请输入修改后的内容" outline />
+            <el-input class="mb-4" v-model="this.nameAlter" placeholder="请输⼊修改后的内容" outline />
           </div>
           <div class="course-title">年龄：
-            <el-input class="mb-4" v-model="this.ageAlter" placeholder="请输入修改后的内容" outline />
+            <el-input class="mb-4" v-model="this.ageAlter" placeholder="请输⼊修改后的内容" outline />
           </div>
           <div class="course-title">邮箱：
-            <el-input class="mb-4" v-model="this.emailAlter" placeholder="请输入修改后的内容" outline />
+            <el-input class="mb-4" v-model="this.emailAlter" placeholder="请输⼊修改后的内容" outline />
           </div>
           <div class="course-title">学校：
-            <el-input class="mb-4" v-model="this.schoolAlter" placeholder="请输入修改后的内容" outline />
+            <el-input class="mb-4" v-model="this.schoolAlter" placeholder="请输⼊修改后的内容" outline />
           </div>
-
-          <div class="course-title">个人介绍：
+          <div class="course-title">个⼈介绍：
             <el-input class="mb-4" type="textarea" :autosize="{ minRows: 5}" v-model="this.contentAlter"
-              placeholder="请输入修改后的内容" outline />
+              placeholder="请输⼊修改后的内容" outline />
           </div>
-
           <el-button @click="submit" color="#e0e5df" style="color: rgb(40,40,40)">确认修改</el-button>
         </el-card-content>
       </el-card>
     </el-dialog>
     <el-container>
-
       <el-aside>
         <img :src="headImgSrc" width="220" height="220" style="border-radius:50%;margin-top:50px">
         <div style="margin-top:-85px;">
           <h2>{{name}}</h2>
         </div>
         <!-- <hr style="margin-top:-40px;"> -->
-
       </el-aside>
-
       <el-main>
         <div class="personalInfoPanel">
           <el-row style="height:40px">
             <el-col :span="20">
-              <h2>个人资料</h2>
+              <h2>个⼈资料</h2>
             </el-col>
             <el-col :span="1">
-              <el-button @click="jumpPage" style="width:130px;height:40px; border-radius:10px;">编辑个人资料</el-button>
+              <el-button @click="jumpPage" style="width:130px;height:40px; border-radius:10px;">编辑个⼈资料</el-button>
             </el-col>
           </el-row>
-
           <br>
-
           <el-col id="info">
             <div class="personalInfoTitle">账号
             </div>
@@ -80,7 +70,6 @@
             <div class="personalInfoTitle">｜
             </div>
           </el-col>
-
           <el-col id="info">
             <div class="personalInfoTitle">邮箱
             </div>
@@ -90,17 +79,15 @@
             <div class="personalInfoTitle">｜
             </div>
           </el-col>
-
-          <!--        <el-col id="info">-->
-          <!--          <div class="personalInfoTitle">所在地-->
-          <!--          </div>-->
-          <!--          <div class="personalInfoContent">-->
-          <!--            {{this.area}}-->
-          <!--          </div>-->
-          <!--          <div class="personalInfoTitle">｜-->
-          <!--          </div>-->
-          <!--        </el-col>-->
-
+          <!-- <el-col id="info">-->
+          <!-- <div class="personalInfoTitle">所在地-->
+          <!-- </div>-->
+          <!-- <div class="personalInfoContent">-->
+          <!-- {{this.area}}-->
+          <!-- </div>-->
+          <!-- <div class="personalInfoTitle">｜-->
+          <!-- </div>-->
+          <!-- </el-col>-->
           <el-col id="info">
             <div class="personalInfoTitle">学校
             </div>
@@ -110,19 +97,16 @@
             <div class="personalInfoTitle">｜
             </div>
           </el-col>
-
           <p>.</p>
           <el-col id="info">
             <div class="personalInfoTitle">
-              个人介绍
+              个⼈介绍
             </div>
             <div class="personalInfoContent" style="white-space: pre-line">
               {{this.content}}
             </div>
           </el-col>
-
         </div>
-
         <div class="question" style="margin-top:140px;">
           <hr style="margin-top:-15px;">
           <h2 style="text-align:left;margin-top:20px;">提交记录</h2>
@@ -130,18 +114,16 @@
             style="width: 100%; padding-left: 13%;padding-right: 5%;border-radius: 20px;"
             :row-class-name="tableRowClassName" :default-sort="{prop:'submitTime',order:'ascending'}">
             <el-table-column label="id" align="center" prop="id" v-if="false" />
-            <el-table-column prop="result" label="状态" sortable width="130">
+            <el-table-column prop="state" label="状态" sortable width="130">
               <template #default="scope">
-                <span v-if="scope.row.result== 'pass'" style="color: lightgreen;">{{scope.row.result}}</span>
-                <span v-else-if="scope.row.result == 'failed'" style="color: lightcoral;">{{scope.row.result}}</span>
-                <span v-else>{{scope.row.result}}</span>
+                <span v-if="scope.row.state == 1" style="color: lightgreen;">pass</span>
+                <span v-else-if="scope.row.state == 0" style="color: lightcoral;">failed</span>
+                <span v-else>failed</span>
               </template>
             </el-table-column>
-            <el-table-column prop="name" label="题目" sortable width="350">
+            <el-table-column prop="name" label="题⽬" sortable width="350">
             </el-table-column>
-
             <el-table-column prop="passingRate" label="通过率" sortable width="100">
-
             </el-table-column>
             <el-table-column prop="difficulty" label="难度" sortable width="100">
               <template #default="scope">
@@ -153,31 +135,26 @@
                 <span v-else>{{scope.row.difficulty}}</span>
               </template>
             </el-table-column>
-            <el-table-column prop="language" label="编程语言" sortable width="120">
+            <el-table-column prop="language" label="编程语⾔" sortable width="120">
             </el-table-column>
             <el-table-column prop="submitTime" label="提交时间" sortable width="130">
             </el-table-column>
           </el-table>
         </div>
       </el-main>
-
     </el-container>
-
     <!-- </div>
-    </div> -->
+ </div> -->
   </div>
 </template>
-
 <script>
 import Modal from './alterMyInfo.vue'
 import axios from 'axios'
-
 export default {
   components: {
     Modal
   },
   data () {
-
     return {
       headImgSrc: 'https://tva2.sinaimg.cn/large/9bd9b167ly1fzjxyujrpaj20b40b40ta.jpg',
       dialogFormVisible: false,
@@ -195,25 +172,23 @@ export default {
       title: '弹窗标题',
       show: true,
       showAvatar: false,
-
       currentID: '',
-
       nameAlter: 'Jackie',
       ageAlter: '21',
       areaAlter: 'SHANGHAI HUANGDU',
-      contentAlter: '较强的动手潜力。良好协调沟通潜力，适应力强，反应快用心灵活，爱创新！两年的会计经历锻炼了我细心的准则。',
-      schoolAlter: '黄渡理工',
+      contentAlter: '较强的动⼿潜⼒。良好协调沟通潜⼒，适应⼒强，反应快⽤⼼灵活，爱创新！两年的会计经历锻炼了我细⼼的准则。',
+      schoolAlter: '⻩渡理⼯',
       emailAlter: '1@gmail.com',
       id: '2821166215',
       name: 'Jackie',
       age: '21',
       area: 'SHANGHAI HUANGDU',
-      content: '较强的动手潜力。良好协调沟通潜力，适应力强，反应快用心灵活，爱创新！两年的会计经历锻炼了我细心的准则。',
-      school: '黄渡理工',
+      content: '较强的动⼿潜⼒。良好协调沟通潜⼒，适应⼒强，反应快⽤⼼灵活，爱创新！两年的会计经历锻炼了我细⼼的准则。',
+      school: '⻩渡理⼯',
       email: '1@gmail.com',
       tags: [
-        { name: '标签一', type: '' },
-        { name: '标签二', type: 'info' },
+        { name: '标签⼀', type: '' },
+        { name: '标签⼆', type: 'info' },
       ],
       difficultyOptions: [{
         value: '困难',
@@ -241,14 +216,14 @@ export default {
       tableData: [{
         id: '1',
         result: '未完成',
-        name: '大整数加法',
+        name: '⼤整数加法',
         submitTime: '2022.01.07',
         passingRate: '50%',
         difficulty: '简单',
       }, {
         id: '1',
         result: '未完成',
-        name: '大整数加法',
+        name: '⼤整数加法',
         solution: '1',
         passingRate: '50%',
         difficulty: '中等',
@@ -256,14 +231,12 @@ export default {
       }, {
         id: '1',
         result: '未完成',
-        name: '大整数加法',
+        name: '⼤整数加法',
         solution: '1',
         passingRate: '50%',
         difficulty: '困难',
         submitTime: '2022.01.07',
       }],
-
-
     }
   },
   methods: {
@@ -273,17 +246,14 @@ export default {
       }
       else if (this.currentID % 4 == 2) {
         this.headImgSrc = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2F92%2F4a%2F72%2F924a726144487f372ac97057dbb01e81.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1644142269&t=289155d2b0f39b19ed108a8b0eb24f8d'
-
       } else if (this.currentID % 4 == 3) {
         this.headImgSrc = 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201704%2F05%2F20170405213655_uSEiT.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1644142269&t=0f75b074995a35b8c4e818116dd79fa8'
-
       }
-
     },
     jumpPage () {
       //alert(this.dialogFormVisible)
       this.dialogFormVisible = true
-      //  alert(this.dialogFormVisible)
+      // alert(this.dialogFormVisible)
       //window.location.href="/hdoj/alterInfo";
     },
     hideModal () {
@@ -304,15 +274,11 @@ export default {
       axios.post(
         "http://localhost:8081/account/updateInfo", param
       ).then(res => {
-
       })
       let _this = this
       setTimeout(function () {
-
         _this.refreshP()
-
       }, 600)
-
       this.dialogFormVisible = false
     },
     updateTmpInfo () {
@@ -321,7 +287,6 @@ export default {
       this.contentAlter = this.content
       this.emailAlter = this.email
       this.schoolAlter = this.school
-
     },
     firstUploadPersonalInfo () {
       // let userLogin
@@ -332,8 +297,6 @@ export default {
       // this.content = userLogin.profile
       // this.email = userLogin.email
       // this.school = userLogin.university
-
-
     },
     refreshP () {
       this.getCurrentId()
@@ -348,18 +311,14 @@ export default {
         this.content = res.data.data.user.profile
         this.email = res.data.data.user.email
         this.school = res.data.data.user.university
-
         this.updateTmpInfo()
-
         this.tableData.splice(0, this.tableData.length)
         for (var i = 0; i < res.data.data.submissionCode.length; i++) {
           this.tableData.push(res.data.data.submissionCode[i])
         }
-
         for (var i = 0; i < res.data.data.questionList.length; i++) {
           this.tableData[i].questionId = res.data.data.questionList[i].questionId
           this.tableData[i].difficulty = res.data.data.questionList[i].difficulty
-
           this.tableData[i].name = res.data.data.questionList[i].name
           if (res.data.data.questionList[i].submission == 0)
             this.tableData[i].passingRate = "--"
@@ -367,9 +326,7 @@ export default {
             this.tableData[i].passingRate = res.data.data.questionList[i].pass / res.data.data.questionList[i].submission
         }
         this.$forceUpdate()
-
       })
-
     },
     openDetails (row) {
       window.localStorage.setItem("questionId", row.questionId)
@@ -381,60 +338,50 @@ export default {
       this.currentID = userInfo.id
     },
     // refreshUserInfo(){
-    //   axios.post(
-    //       "http://localhost:8081/account/accountInfo"
-    //   ).then(res => {
-    //     this.tableData.splice(0,this.tableData.length)
-    //     console.log(res)
-    //     // for(var i = 0;i<res.data.submissionCode.length();i++)
-    //     // {
-    //     //   this.tableData.push()
-    //     //   res.data.submissionCode
-    //     //
-    //     // }
+    // axios.post(
+    // "http://localhost:8081/account/accountInfo"
+    // ).then(res => {
+    // this.tableData.splice(0,this.tableData.length)
+    // console.log(res)
+    // // for(var i = 0;i<res.data.submissionCode.length();i++)
+    // // {
+    // // this.tableData.push()
+    // // res.data.submissionCode
+    // //
+    // // }
     //
-    //     for(var i = 0;i<res.data.questionNames.length;i++)
-    //     {
-    //       this.tableData[i].name = res.data.questionNames[i]
-    //     }
-    //     this.$forceUpdate()
-    //   })
+    // for(var i = 0;i<res.data.questionNames.length;i++)
+    // {
+    // this.tableData[i].name = res.data.questionNames[i]
+    // }
+    // this.$forceUpdate()
+    // })
     // }
   },
-
   mounted () {
     this.getCurrentId()
     this.updateTmpInfo()
     this.refreshP()
     this.updateHeadImg()
-
-
     // axios.post(
-    //     "http://localhost:8081/account/accountInfo"
+    // "http://localhost:8081/account/accountInfo"
     // ).then(res => {
-    //   console.log(res)
-    //   // for(var i = 0;i<res.data.submissionCode.length();i++)
-    //   // {
-    //   //   this.tableData.push()
-    //   //   res.data.submissionCode
-    //   //
-    //   // }
-    //   for(var i = 0;i<res.data.questionNames.length();i++)
-    //   {
-    //     this.tableData[i].name = res.data.questionNames
-    //   }
-    //   this.$forceUpdate()
-
+    // console.log(res)
+    // // for(var i = 0;i<res.data.submissionCode.length();i++)
+    // // {
+    // // this.tableData.push()
+    // // res.data.submissionCode
+    // //
+    // // }
+    // for(var i = 0;i<res.data.questionNames.length();i++)
+    // {
+    // this.tableData[i].name = res.data.questionNames
+    // }
+    // this.$forceUpdate()
     //})
-
-
   }
-
-
-
 }
 </script>
-
 <style scoped>
 .photo {
   margin-bottom: 20px;
@@ -460,7 +407,6 @@ export default {
   text-align: center;
   line-height: 100px;
 }
-
 .el-main {
   background-color: #e9eef3;
   color: var(--el-text-color-primary);
@@ -473,17 +419,16 @@ export default {
 }
 .personalInfoPanel {
 }
-
 /* body > .el-container {
-  margin-bottom: 50px;
+ margin-bottom: 50px;
 } */
 .el-row {
   margin-bottom: 20px;
   height: 90px;
   text-align: left;
   /* &:last-child {
-    margin-bottom: 0;
-  } */
+ margin-bottom: 0;
+ } */
 }
 .el-col {
   border-radius: 4px;
@@ -499,19 +444,16 @@ export default {
   text-align: right;
 }
 /* .grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+ border-radius: 4px;
+ min-height: 36px;
 } */
-
 /* .el-container:nth-child(5) .el-aside,
 .el-container:nth-child(6) .el-aside {
-  line-height: 260px;
+ line-height: 260px;
 }
-
 .el-container:nth-child(7) .el-aside {
-  line-height: 320px;
+ line-height: 320px;
 } */
-
 .personalInfoTitle {
   width: 180px;
   font-size: 16px;
@@ -524,7 +466,6 @@ export default {
   margin-right: 6px;
   margin-left: 6px;
 }
-
 .personalInfoContent {
   font-size: 16px;
   line-height: 20px;
